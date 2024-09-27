723c066cdc021ad5cbf8fdd5ae594296e8ac8909
Vladimir Nikishin <nickysheen@yandex.ru>
2024-09-27 21:20:12 +0200

    chore: Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ build_command
 
 # Создаем .gitignore в папке dist, чтобы исключить ненужные файлы
 echo -e "\033[0;32mCreating .gitignore to exclude unnecessary files...\033[0m"
-echo -e ".gitignore\n.editorconfig" > $directory/.gitignore
+echo -e ".gitignore\n.editorconfig\n\$RECYCLE.BIN/\n*.DS_Store\n*.app\n*.bak\n*.code-workspace\n*.log\n*.sass-cache*\n*.swp\n*_notes\n.DocumentRevisions-V100\n.Spotlight-V100\n.TemporaryItems\n.Trashes\n.VolumeIcon.icns\n._*\n.com.apple.timemachine.donotpresent\n.env\n.env.*\n.eslintcache\n.fseventsd\n.npm\n.publish\n.tmp\n.vscode/*\n.yarn\n.yarn\n.yarn-integrity\nDesktop.ini\nThumbs.db\nbackup\ndeploy.sh\ndist\nehthumbs.db\nehthumbs_vista.db\ngulpfile.babel.js\njspm_packages\nnode_modules\nnpm-debug.log*\npackage.json\nsrc\nyarn-debug.log*\nyarn-error.log*\nyarn.lock\n" > $directory/.gitignore
 
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
 cd $directory &&

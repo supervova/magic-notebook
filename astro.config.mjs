b3acf9305181d5f5992e05f336a1d564b9e7d1a6
Vladimir Nikishin <nickysheen@yandex.ru>
2026-04-16 18:10:05 +0200

    chore: prepare seo metadata for magic-notebook.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://magic-nb.ru',
+  site: 'https://magic-notebook.com',
   output: 'static',
   i18n: {
     defaultLocale: 'en',

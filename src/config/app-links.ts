296929d56d93050ebe7c99fc2d59f96dcfc30971
Vladimir Nikishin <nickysheen@yandex.ru>
2026-06-30 22:32:34 +0200

    Update app-links.ts

--- src/config/app-links.ts
+++ src/config/app-links.ts
@@ -2,8 +2,8 @@ export const appLinks = {
   latestRelease: 'https://github.com/Magic-Notebook/App/releases/latest',
   downloads: {
     macos:
-      'https://github.com/Magic-Notebook/updates/releases/download/v1.1.1/MagicNotebook-1.1.1.dmg',
-    // 'https://github.com/Magic-Notebook/updates/releases/download/v1.1.3/MagicNotebook-1.1.3.dmg',
+      // 'https://github.com/Magic-Notebook/updates/releases/download/v1.1.1/MagicNotebook-1.1.1.dmg',
+      'https://github.com/Magic-Notebook/updates/releases/download/v1.1.3/MagicNotebook-1.1.3.dmg',
     windows: 'https://apps.microsoft.com/detail/9nb0qng2kd8z',
   },
 } as const;
